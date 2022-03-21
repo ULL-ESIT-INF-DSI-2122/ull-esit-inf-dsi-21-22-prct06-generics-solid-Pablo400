fd19954f7d008a74c52fa1aa9e745c8ce7904ff9
Pablo400 <alu0101311135@ull.edu.es>
2022-03-21 22:02:59 +0000

    Empezar Ejercicio 1

--- code-pr06/dist/ejercicio-1.js
+++ code-pr06/dist/ejercicio-1.js
@@ -1,1 +1,155 @@
 "use strict";
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+class Pokemon {
+    constructor(name, height, weight, type, attack, defense, velocity, health) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.type = type;
+        this.attack = attack;
+        this.defense = defense;
+        this.velocity = velocity;
+        this.health = health;
+    }
+    getName() {
+        return this.name;
+    }
+    getHeight() {
+        return this.height;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    getType() {
+        return this.type;
+    }
+    getAttack() {
+        return this.attack;
+    }
+    getDefense() {
+        return this.defense;
+    }
+    getVelocity() {
+        return this.velocity;
+    }
+    getHealth() {
+        return this.health;
+    }
+    printPokemonData() {
+        return `${this.getName()} es un pokemon de tipo ${this.getType()}, pesa ${this.getWeight()} kg y mide ${this.getHeight()} m.Tiene ${this.getAttack()} puntos de ataque, ${this.getDefense()} puntos de defensa, ${this.getVelocity()} puntos de velocidad y ${this.getHealth()} puntos de vida`;
+    }
+}
+class Jojo {
+    constructor(name, height, weight, type, attack, defense, velocity, health, standName, specialPower, range, destructivePower, stamina, precision, developmentPotencial) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.type = type;
+        this.attack = attack;
+        this.defense = defense;
+        this.velocity = velocity;
+        this.health = health;
+        this.standName = standName;
+        this.specialPower = specialPower;
+        this.range = range;
+        this.destructivePower = destructivePower;
+        this.stamina = stamina;
+        this.precision = precision;
+        this.developmentPotencial = developmentPotencial;
+    }
+    getName() {
+        return this.name;
+    }
+    getHeight() {
+        return this.height;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    getType() {
+        return this.type;
+    }
+    getAttack() {
+        return this.attack;
+    }
+    getDefense() {
+        return this.defense;
+    }
+    getVelocity() {
+        return this.velocity;
+    }
+    getHealth() {
+        return this.health;
+    }
+    getStandName() {
+        return this.standName;
+    }
+    getSpecialPower() {
+        return this.specialPower;
+    }
+    getRange() {
+        return this.range;
+    }
+    getDestructivePower() {
+        return this.destructivePower;
+    }
+    getStamina() {
+        return this.stamina;
+    }
+    getPrecision() {
+        return this.precision;
+    }
+    getDevelopmentPotencial() {
+        return this.developmentPotencial;
+    }
+    printJojoData() {
+        const firstData = `${this.getName()} tiene un stand de tipo ${this.getType()}, ${this.getName()} pesa ${this.getWeight()} kg y mide ${this.getHeight()} m. Su stand ${this.getStandName()} tiene ${this.getAttack()} puntos de ataque, ${this.getDefense()} puntos de defensa, ${this.getVelocity()} puntos de velocidad y ${this.getName()} tiene ${this.getHealth()} vida.`;
+        const secondData = `Otros datos de interés del stand son su poder especial ${this.getSpecialPower()}, distancia de efecto ${this.getRange()}, su poder destructivo ${this.getDestructivePower()}, su stamina ${this.getStamina()}, su precisión ${this.getPrecision()} y su potencial de mejora ${this.getDevelopmentPotencial()}`;
+        return `${firstData} ${secondData}`;
+    }
+}
+class SaveFighter {
+    constructor() { }
+    ;
+    saveFighter(...Fighter) {
+        const fighterSave = [];
+        Fighter.forEach((fighter) => {
+            fighterSave.push(fighter);
+        });
+        return fighterSave;
+    }
+}
+class Fighter {
+    constructor(firstFighter, secondFighter) {
+        this.firstFighter = firstFighter;
+        this.secondFighter = secondFighter;
+    }
+}
+class Combat extends Fighter {
+    constructor(firstFighter, secondFighter) {
+        super(firstFighter, secondFighter);
+        this.firstFighter = firstFighter;
+        this.secondFighter = secondFighter;
+    }
+    getFirstFighter() {
+        return this.firstFighter;
+    }
+    getSecondFighter() {
+        return this.secondFighter;
+    }
+    getDamage(Fighter) {
+        return 0;
+    }
+    startFight(Fighter1, Fighter2) {
+        return 0;
+    }
+}
+const Jotaro = new Jojo('Jotaro', 1.95, 81, 'Corto Alcance', 200, 50, 200, 100, 'Star Platinum', 'The World', 'C', 'A', 'A', 'A', 'A');
+const Pikachu = new Pokemon('Pikachu', 0.4, 6, 'eléctrico', 55, 40, 90, 35);
+const Totodile = new Pokemon('Totodile', 0.6, 9.5, 'agua', 65, 64, 43, 50);
+console.log(Jotaro.printJojoData());
+console.log(Pikachu.printPokemonData());
+const Fighters = new SaveFighter();
+console.log(Fighters.saveFighter(Jotaro, Pikachu, Totodile));
---
+++ code-pr06/src/ejercicio-1.ts
@@ -0,0 +1,178 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+
+class Pokemon {
+  constructor(private name : string, private height : number,
+    private weight : number, private type : string, private attack : number,
+    private defense : number, private velocity : number, private health : number) { }
+
+  getName() {
+    return this.name;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getWeight() {
+    return this.weight;
+  }
+
+  getType() {
+    return this.type;
+  }
+
+  getAttack() {
+    return this.attack;
+  }
+
+  getDefense() {
+    return this.defense;
+  }
+
+  getVelocity() {
+    return this.velocity;
+  }
+
+  getHealth() {
+    return this.health;
+  }
+
+  printPokemonData() {
+    return `${this.getName()} es un pokemon de tipo ${this.getType()}, pesa ${this.getWeight()} kg y mide ${this.getHeight()} m.Tiene ${this.getAttack()} puntos de ataque, ${this.getDefense()} puntos de defensa, ${this.getVelocity()} puntos de velocidad y ${this.getHealth()} puntos de vida`;
+  }
+}
+
+class Jojo {
+  constructor(private name : string, private height : number,
+    private weight : number, private type : string, private attack : number,
+    private defense : number, private velocity : number, private health : number,
+    private standName : string, private specialPower : string, private range : string,
+    private destructivePower : string, private stamina : string, private precision : string,
+    private developmentPotencial : string) { }
+
+  getName() {
+    return this.name;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getWeight() {
+    return this.weight;
+  }
+
+  getType() {
+    return this.type;
+  }
+
+  getAttack() {
+    return this.attack;
+  }
+
+  getDefense() {
+    return this.defense;
+  }
+
+  getVelocity() {
+    return this.velocity;
+  }
+
+  getHealth() {
+    return this.health;
+  }
+
+  getStandName() {
+    return this.standName;
+  }
+
+  getSpecialPower() {
+    return this.specialPower;
+  }
+
+  getRange() {
+    return this.range;
+  }
+
+  getDestructivePower() {
+    return this.destructivePower;
+  }
+
+  getStamina() {
+    return this.stamina;
+  }
+
+  getPrecision() {
+    return this.precision;
+  }
+
+  getDevelopmentPotencial() {
+    return this.developmentPotencial;
+  }
+
+  printJojoData() {
+    const firstData : string = `${this.getName()} tiene un stand de tipo ${this.getType()}, ${this.getName()} pesa ${this.getWeight()} kg y mide ${this.getHeight()} m. Su stand ${this.getStandName()} tiene ${this.getAttack()} puntos de ataque, ${this.getDefense()} puntos de defensa, ${this.getVelocity()} puntos de velocidad y ${this.getName()} tiene ${this.getHealth()} vida.`;
+    const secondData : string= `Otros datos de interés del stand son su poder especial ${this.getSpecialPower()}, distancia de efecto ${this.getRange()}, su poder destructivo ${this.getDestructivePower()}, su stamina ${this.getStamina()}, su precisión ${this.getPrecision()} y su potencial de mejora ${this.getDevelopmentPotencial()}`;
+    return `${firstData} ${secondData}`;
+  }
+}
+
+class SaveFighter<Universe> {
+  constructor() {};
+
+  saveFighter(...Fighter : Universe[]) {
+    const fighterSave : Universe[] = [];
+    Fighter.forEach((fighter) => {
+      fighterSave.push(fighter);
+    });
+
+    return fighterSave;
+  }
+}
+
+abstract class Fighter<U1, U2> {
+  constructor(protected firstFighter : U1, protected secondFighter : U2) { }
+
+  abstract getFirstFighter() : U1;
+
+  abstract getSecondFighter() : U2;
+
+  abstract getDamage(Fighter : U1 |U2) : number;
+
+  abstract startFight(Fighter1 : U1, Fighter2 : U2) : number;
+}
+
+class Combat<U1, U2> extends Fighter<U1, U2> {
+  constructor(protected firstFighter : U1, protected secondFighter : U2) {
+    super(firstFighter, secondFighter);
+  }
+
+  getFirstFighter(): U1 {
+    return this.firstFighter;
+  }
+
+  getSecondFighter(): U2 {
+    return this.secondFighter;
+  }
+
+  getDamage(Fighter: any): number {
+    return 0;
+  }
+
+  startFight(Fighter1: any, Fighter2 : any): number {
+    return 0;
+  }
+}
+
+const Jotaro = new Jojo('Jotaro', 1.95, 81, 'Corto Alcance', 200, 50, 200, 100, 'Star Platinum', 'The World', 'C', 'A', 'A', 'A', 'A');
+const Pikachu = new Pokemon('Pikachu', 0.4, 6, 'eléctrico', 55, 40, 90, 35);
+const Totodile = new Pokemon('Totodile', 0.6, 9.5, 'agua', 65, 64, 43, 50);
+
+console.log(Jotaro.printJojoData());
+console.log(Pikachu.printPokemonData());
+
+const Fighters = new SaveFighter();
+
+console.log(Fighters.saveFighter(Jotaro, Pikachu, Totodile));
